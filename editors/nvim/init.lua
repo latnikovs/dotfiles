1a328e4235eb49d5eccb9bfd43155213f2fa8715
Dmitrijs Latņikovs <latnikov@gmail.com>
2026-02-19 14:29:48 +0200

    feat: use catppucin/macchiato theme for nvim

--- editors/nvim/init.lua
+++ editors/nvim/init.lua
@@ -866,29 +866,23 @@ require("lazy").setup({
 		},
 	},
 
-	{ -- You can easily change to a different colorscheme.
-		-- Change the name of the colorscheme plugin below, and then
-		-- change the command in the config to whatever the name of that colorscheme is.
-		--
-		-- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-		"folke/tokyonight.nvim",
-		priority = 1000, -- Make sure to load this before all the other start plugins.
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
 		config = function()
-			---@diagnostic disable-next-line: missing-fields
-			require("tokyonight").setup({
-				on_highlights = function(hl, c)
-					hl.NormalFloat = { bg = c.bg_dark }
-					hl.FloatBorder = { fg = c.blue, bg = c.bg_dark }
-				end,
-				styles = {
-					comments = { italic = false }, -- Disable italics in comments
+			require("catppuccin").setup({
+				flavour = "macchiato",
+				integrations = {
+					gitsigns = true,
+					treesitter = true,
+					telescope = true,
+					neotree = true,
+					mason = true,
+					which_key = true,
 				},
 			})
-
-			-- Load the colorscheme here.
-			-- Like many other themes, this one has different styles, and you could load
-			-- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-			vim.cmd.colorscheme("tokyonight-night")
+			vim.cmd.colorscheme("catppuccin")
 		end,
 	},
 
