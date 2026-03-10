0768be994845d2363cfe5fe6caf1dde1a44cfe1e
Dmitrijs Latņikovs <latnikov@gmail.com>
2026-03-10 17:25:30 +0200

    feat: add dbui

--- editors/nvim/lua/custom/plugins/init.lua
+++ editors/nvim/lua/custom/plugins/init.lua
@@ -2,4 +2,31 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+	"tpope/vim-dadbod",
+	"kristijanhusak/vim-dadbod-completion",
+	{
+		"kristijanhusak/vim-dadbod-ui",
+		dependencies = {
+			"tpope/vim-dadbod",
+			"kristijanhusak/vim-dadbod-completion",
+		},
+		cmd = { "DBUI", "DBUIToggle", "DBUIAddConnection", "DBUIFindBuffer" },
+		keys = {
+			{ "<leader>db", "<cmd>DBUIToggle<cr>", desc = "Toggle DB UI" },
+		},
+		init = function()
+			vim.api.nvim_create_autocmd("FileType", {
+				pattern = { "sql", "mysql", "plsql", "pgsql", "psql", "postgres" },
+				callback = function(event)
+					vim.bo.omnifunc = "vim_dadbod_completion#omni"
+					vim.keymap.set("i", "<C-Space>", "<C-x><C-o>", {
+						buffer = event.buf,
+						silent = true,
+						desc = "Dadbod omni completion",
+					})
+				end,
+			})
+		end,
+	},
+}
